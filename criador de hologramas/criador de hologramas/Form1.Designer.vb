a004b8213c5bc522f8242703fddaaac0de6272e3
JOÃO VITOR <118869806+Joao-J@users.noreply.github.com>
2025-01-23 17:04:12 -0300

    primeiro ainda esta em teste
    cheio de bugs ainda

---
+++ criador de hologramas/criador de hologramas/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        ColorDialog1 = New ColorDialog()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        PictureBox1 = New PictureBox()
+        Timer1 = New Timer(components)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(384, 500)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "exportar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Red
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Location = New Point(12, 500)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(25, 23)
+        Button2.TabIndex = 1
+        Button2.Tag = "cor"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Black
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.Location = New Point(43, 500)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(25, 23)
+        Button3.TabIndex = 2
+        Button3.Tag = "cor"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.Blue
+        Button4.FlatStyle = FlatStyle.Flat
+        Button4.Location = New Point(74, 500)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(25, 23)
+        Button4.TabIndex = 3
+        Button4.Tag = "cor"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.White
+        Button5.FlatStyle = FlatStyle.Flat
+        Button5.Location = New Point(105, 500)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(25, 23)
+        Button5.TabIndex = 4
+        Button5.Tag = "cor"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.Yellow
+        Button6.FlatStyle = FlatStyle.Flat
+        Button6.Location = New Point(136, 500)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(25, 23)
+        Button6.TabIndex = 5
+        Button6.Tag = "cor"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.Orange
+        Button7.FlatStyle = FlatStyle.Flat
+        Button7.Location = New Point(167, 500)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(25, 23)
+        Button7.TabIndex = 6
+        Button7.Tag = "cor"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.Gray
+        Button8.FlatStyle = FlatStyle.Flat
+        Button8.Location = New Point(198, 500)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(25, 23)
+        Button8.TabIndex = 7
+        Button8.Tag = "cor"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.Green
+        Button9.FlatStyle = FlatStyle.Flat
+        Button9.Location = New Point(229, 500)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(25, 23)
+        Button9.TabIndex = 8
+        Button9.Tag = "cor"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.Lime
+        Button10.FlatStyle = FlatStyle.Flat
+        Button10.Location = New Point(260, 500)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(25, 23)
+        Button10.TabIndex = 9
+        Button10.Tag = "cor"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.Magenta
+        Button11.FlatStyle = FlatStyle.Flat
+        Button11.Location = New Point(291, 500)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(25, 23)
+        Button11.TabIndex = 10
+        Button11.Tag = "cor"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.Plum
+        Button12.FlatStyle = FlatStyle.Flat
+        Button12.Location = New Point(322, 500)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(25, 23)
+        Button12.TabIndex = 11
+        Button12.Tag = "cor"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.Cyan
+        Button13.FlatStyle = FlatStyle.Flat
+        Button13.Location = New Point(353, 500)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(25, 23)
+        Button13.TabIndex = 12
+        Button13.Tag = "cor"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Orange
+        PictureBox1.Location = New Point(311, 142)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(15, 15)
+        PictureBox1.TabIndex = 13
+        PictureBox1.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        ClientSize = New Size(466, 535)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+
+End Class
---
+++ criador de hologramas/criador de hologramas/Form1.vb
@@ -0,0 +1,121 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim controles As List(Of Control)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ndp As Integer = 1
+
+        For i As Integer = 1 To 20
+            For i2 As Integer = 1 To 20
+                Dim pixel As New PictureBox
+                With pixel
+                    .Width = 20
+                    .Height = 20
+                    .BackColor = Color.White
+                    .Tag = "pic"
+                    .Top = i * 20 + (1 * i)
+                    .Left = i2 * 20 + (1 * i2)
+                    .Tag &= ndp
+                    .Name = .Tag
+                End With
+                Me.Controls.Add(pixel)
+                ndp += 1
+            Next
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim texto As String = vbNewLine & "novo" & vbNewLine & " - lines:"
+        Dim linha As String = ""
+        Dim ndl As Integer = 1
+
+        For i As Integer = 1 To 400
+            If ndl = 20 Then
+                ndl = 1
+                texto = texto & vbNewLine & linha
+                linha = ""
+            Else
+                Dim nome As String = "pic" & i
+                linha &= retornarcor(Me.Controls.Find(nome, True)(0))
+                ndl += 1
+            End If
+        Next
+
+        If File.Exists(CurDir() & "/tenata/resultado.txt") = True Then
+            IO.File.AppendAllText(CurDir() & "/tenata/resultado.txt", texto)
+        Else
+            If Not Path.Exists(CurDir() & "/tenata/") Then
+                MkDir(CurDir() & "/tenata/")
+            End If
+            File.Create(CurDir() & "/tenata/resultado.txt").Close()
+                IO.File.CreateText(CurDir() & "/tenata/resultado.txt").Close()
+                IO.File.AppendAllText(CurDir() & "/tenata/resultado.txt", texto)
+            End If
+
+    End Sub
+
+    Public Function retornarcor(c As Control)
+        Dim cor As String = ""
+        Select Case c.BackColor
+            Case Color.White
+                cor = "f"
+            Case Color.Blue
+                cor = "1"
+            Case Color.Red
+                cor = "4"
+            Case Color.Green
+                cor = "2"
+            Case Color.Yellow
+                cor = "e"
+            Case Color.Orange
+                cor = "6"
+            Case Color.Gray
+                cor = "8"
+            Case Color.Lime
+                cor = "a"
+            Case Color.Magenta
+                cor = "5"
+            Case Color.Pink
+                cor = "d"
+            Case Color.Cyan
+                cor = "3"
+            Case Color.Black
+                cor = "0"
+            Case Else
+                cor = "f"
+        End Select
+        Return "&" & cor & "▉"
+    End Function
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        PictureBox1.Left = Cursor.Position.X - Me.Left - 15
+        PictureBox1.Top = Cursor.Position.Y - Me.Top - 40
+
+    End Sub
+
+    Private Sub PictureBox1_MouseClick(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseClick
+        Try
+
+            For Each c As Control In Me.Controls
+                If c.Tag <> vbNullString Then
+                    If PictureBox1.Bounds.IntersectsWith(c.Bounds) Then
+                        If c.Tag.ToString.Contains("cor") Then
+                            PictureBox1.BackColor = c.BackColor
+                        Else
+                            If e.Button = MouseButtons.Right Then
+                                c.BackColor = Color.White
+                            ElseIf e.Button = MouseButtons.Left Then
+                                c.BackColor = PictureBox1.BackColor
+                            End If
+                        End If
+                    End If
+                End If
+
+            Next
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
